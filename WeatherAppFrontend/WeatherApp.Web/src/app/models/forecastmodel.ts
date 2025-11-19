ea12f9b477518c83474fcd217bdbec4a6638fc04
Silvio98 <silvio06998@gmail.com>
2025-11-19 14:48:40 +0000

    Added LocationModel and ForecastModel Typescripts

---
+++ WeatherAppFrontend/WeatherApp.Web/src/app/models/forecastmodel.ts
@@ -0,0 +1,10 @@
+export interface ForecastModel{
+    TempMin: number;
+    TempMax: number;
+    ClassWind: string;
+    WindDirection: string;
+    ProbPrecipitation: number;
+    ClassPrecipitation: string;
+    IdWeatherType: number;
+    Diurno: boolean;
+}
---
+++ WeatherAppFrontend/WeatherApp.Web/src/app/models/locationmodel.ts
@@ -0,0 +1,7 @@
+export interface LocationModel{
+    IdRegiao: number;
+    GlobalIdLocal: number;
+    IdConcelho: number;
+    IdDistrito: number;
+    NomeDistrito: string;
+}
